4937ca5d068db504e118671d5df66cb739c0d306
chahat2725 <anurag27.bhandari@simplilearn.net>
2023-05-07 19:54:58 +1000

    main

---
+++ Arraylist.java
@@ -0,0 +1,11 @@
+public class Arraylist {
+    public static void main(String[] args){
+        Array list<string> list = new Array list<String>();
+        list.add("Mango");
+        list.add("kiwi");
+        list.add("grapes");
+        list.add("apple");
+        //printing the analytics object
+        System.out.println(list);
+    }
+}
