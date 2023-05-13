fc6c9ed63ddf02f6c33f073dd408d9075a819cd8
chahat2725 <anurag27.bhandari@simplilearn.net>
2023-05-13 15:19:28 +1000

    class file

---
+++ Array iterator.java
@@ -0,0 +1,13 @@
+public class iterator {
+    public static void main(String[] args){
+        Array list <String> list=new Array list <String>
+        list.add("Mango");
+        list.add("Banana");
+        list.add("Apple");
+        list.add("kiwi");
+        //Traversing the iterator
+        iterator itr= list.iterator();
+        while(it has.next);
+        System.out.println("itr.next");
+    }
+}
---
+++ for loop.java
@@ -0,0 +1,12 @@
+public class loop {
+    public static void main(String[] args){
+        Array list<String> list = new Array list<String>();
+        list.add("Mango");
+        list.add("Banana");
+        list.add("Kiwi");
+        list.add("orange");
+        //Traversing list through for each loop
+        for (String fruit.list)
+        System.out.println("fruit");
+    }
+}
